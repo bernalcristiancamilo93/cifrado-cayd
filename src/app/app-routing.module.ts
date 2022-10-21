7a17bcd29c1a44e814cd9673d5f0e235023fe0b5
Camilo Bernal <camilo.bernal@aciworldwide.com>
2022-10-21 03:13:08 -0500

    Updated app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,12 +16,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'cesar',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: 'cesar',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
 ];
